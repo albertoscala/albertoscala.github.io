84803b8f2783e3bd627fb0fd618d0e9c01471cc1
Alberto Scala <31989626+albertoscala@users.noreply.github.com>
2025-06-16 08:21:05 +0300

    cleaning

--- README.md
+++
@@ -1,1 +0,0 @@
-# albertoscala.github.io
--- main.js
+++
@@ -1,7 +0,0 @@
-function changeTheme() {
-    const root = document.documentElement;
-    const currentTheme = root.classList.contains('light') ? 'light' : 'dark';
-    const newTheme = currentTheme === 'light' ? 'dark' : 'light';
-    root.classList.toggle('light', newTheme === 'light');
-    document.getElementById('mode-toggle').textContent = newTheme === 'light' ? '☀️' : '🌙';
-}
